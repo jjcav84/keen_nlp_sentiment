af2ead39449fe870454bb8c55e42cd9be02c3afb
jjcav84 <jjcav84@gmail.com>
2022-07-28 22:32:45 -0500

    updated for mixpanel implementation v13

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ exports.anthemcNLP = (req, res) => {
   /* this handles CORS preflight check */
   if (req.method === 'OPTIONS') {
     // Send response to OPTIONS requests
-    res.set('Access-Control-Allow-Methods', 'GET, POST');
+    res.set('Access-Control-Allow-Methods', 'POST');
     res.set('Access-Control-Allow-Headers', 'Content-Type');
     res.set('Access-Control-Max-Age', '3600');
     res.status(204).send('');
